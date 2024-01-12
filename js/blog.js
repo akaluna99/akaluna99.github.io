260c91b6a61d58a23156d3e4a400ba4053996276
rubn99 <ruben.moreno.romero@gmail.com>
2024-01-12 19:39:09 +0100

    bloggeee

--- js/blog.js
+++ js/blog.js
@@ -1,21 +1,27 @@
 
-fetch('../blog/index.json')
+//Nem a buscar el index del blog
+fetch('/blog/index.json')
     .then((response) => response.json())
     .then((jsonIndex) => {
-        // READ THE BLOG INDEX
-        document.getElementById("title").innerHTML = jsonIndex[0].title
-        fetch(jsonIndex[0].path)
+        // Agafem el primer registre i el posem al titol (No el vull)
+        //document.getElementById("title").innerHTML = jsonIndex[0].title;
+        // A el valor de path tenim el path a el json del blog
+        fetch(jsonIndex[0].path + '.json')
           .then((response) => response.json())
           .then((json) => {
-              // READ THE BLOG INDEX
+              // Llegim el titul de DINS del blog
               document.getElementById("title").innerHTML = json.title
               
+              // Si el contingun es mes llarg de 50 caracters el posem sencer
               if (json.content.length < 50){
                 document.getElementById("content").innerHTML = json.content;
               } else {
+                // Sino l'acortem (perque vas posar 97 luna? tho deixo igual pero wtf)
                 document.getElementById("content").innerHTML = json.content.slice(0, 97) + "..."; 
-                document.getElementById("content").innerHTML += `<br><br>
-                  <button onclick="window.location.assign('/blog/?blog=${jsonIndex[0].path.substring(6)}');">Llegir més</button>`
               }
+              // Creem un boto de llegir mes, que portara a la pagina del blog i mitjançant magia fara apareixer l'article com pop up
+              // (La magia es posarho com a valor de la url (nose perque faig un substring de 6...))
+              document.getElementById("content").innerHTML += `<br><br>
+                <button onclick="window.location.assign('/blog/?blog=${jsonIndex[0].path.substring(6)}');">Llegir més</button>`;
           });
     });
--- js/blog_page.js
+++ js/blog_page.js
@@ -1,34 +1,49 @@
 
 function mostrarMes(data) {
-  //console.log(data)
-  fetch(data)
+  console.log("mostra mes", data)
+  /*fetch(data + '.json')
     .then((response) => response.json())
     .then((json) => {
-      //console.log(json)
+      console.log(json)
       document.getElementById(`post_${data}`).innerHTML = 
       ` <h2>${json['title']}</h2> 
         ${json['content']}
         <hr>
         <figcaption>—${json["date"]}</figcaption>`
+    })*/
+    let stateObj = { id: "100" };
+    window.history.pushState(stateObj, "Page 2", '?blog='+data.split('/blog/')[1]);
+    fetch(data + '.json')
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      document.getElementById(`modal`).innerHTML = 
+      ` <h2>${json['title']}</h2> 
+        ${json['content']}
+        <hr>
+        <figcaption>—${json["date"]}</figcaption>
+        <figcaption>${json["author"]}</figcaption>`
     })
+    document.querySelector("dialog").showModal(); 
 }
 
-fetch('../blog/index.json')
+fetch('/blog/index.json')
   .then((response) => response.json())
   .then((jsonIndex) => {
-      // READ THE BLOG INDEX TITLES
+      // Nem a buscar dins del index tots els PATHS
       jsonIndex.forEach(obj => {
         Object.entries(obj).forEach(([key, value]) => {
-            //console.log(`${key} ${value}`);
+            console.log(`${key} ${value}`);
         });
-        //console.log('-------------------');
+        console.log('-------------------');
         document.getElementById("blog_wrapper").innerHTML += 
-      `<br><div class="blog_preview" id='post_${obj['path']}'>
-            <h2>${obj['title']}</h2>`
-        fetch(obj['path'])
+      `<br><div class="blog_preview" id='post_${obj['path']}'>`
+            //<h2>${obj['title']}</h2>`
+        fetch(obj['path'] + '.json')
           .then((response) => response.json())
           .then((json) => {
               //console.log(json)
+              document.getElementById(`post_${obj['path']}`).innerHTML += `<h2>${json['title']}</h2>`;
               if (json['content'].length < 97){
                 document.getElementById(`post_${obj['path']}`).innerHTML += 
                 `${json['content']}
@@ -39,10 +54,11 @@ fetch('../blog/index.json')
               } else {
                 document.getElementById(`post_${obj['path']}`).innerHTML += 
                 `${json['content'].slice(0, 97) + '...'}
-                  <br><br>
+                  <br>
                   <button onclick="mostrarMes('${obj['path']}')">Llegir més</button>
                   <hr>
                   <figcaption>—${json["date"]}</figcaption>
+                  <figcaption>${json["author"]}</figcaption>
                   </div> <!-- WRAPPER --> 
                 `;
               }
@@ -53,7 +69,8 @@ fetch('../blog/index.json')
   const urlParams = new URLSearchParams(queryString);
   console.log(urlParams.get('blog'));
   if (urlParams.get('blog')){
-    fetch('/blog/' + urlParams.get('blog'))
+    mostrarMes('/blog/' + urlParams.get('blog'));
+    /*fetch('/blog/' + urlParams.get('blog'))
     .then((response) => response.json())
     .then((json) => {
       console.log(json)
@@ -63,5 +80,5 @@ fetch('../blog/index.json')
         <hr>
         <figcaption>—${json["date"]}</figcaption>`
     })
-    document.querySelector("dialog").showModal(); 
+    document.querySelector("dialog").showModal(); */
   }
