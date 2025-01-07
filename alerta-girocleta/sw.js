cd31c2fad1d8ecfafbc3c15837b2299c84f83806
luna <akaluna99@gmail.com>
2025-01-07 12:08:36 +0100

    upg

---
+++ alerta-girocleta/sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/icon.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request);
+        }
+      })
+    );
+  });
+  
+  self.addEventListener('push', function(event) {
+    var data = event.data.json();
+    event.waitUntil(
+      self.registration.showNotification(data.title, {
+        body: data.message,
+        icon: 'icon.png'
+      })
+    );
+  });
+  
